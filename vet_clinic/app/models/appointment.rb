7d9ea6ce896223875359524252ea94cde84bf55c
Domingo De la Fuente <ddelafuente@miuandes.cl>
2026-04-29 20:36:53 -0400

    Add enum for appointment status

--- vet_clinic/app/models/appointment.rb
+++ vet_clinic/app/models/appointment.rb
@@ -3,6 +3,13 @@ class Appointment < ApplicationRecord
   belongs_to :vet
   has_many :treatments
 
+  enum :status, {
+    scheduled: 0,
+    in_progress: 1,
+    completed: 2,
+    cancelled: 3
+  }
+
   validates :date, presence: true
   validates :reason, presence: true
   validates :pet, presence: true
