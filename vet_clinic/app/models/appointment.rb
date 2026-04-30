ee9dbea2a482f1dd923ac6c1288687ba0b54afac
Domingo De la Fuente <ddelafuente@miuandes.cl>
2026-04-29 20:17:53 -0400

    Add validations to Vet and Appointment models

--- vet_clinic/app/models/appointment.rb
+++ vet_clinic/app/models/appointment.rb
@@ -2,4 +2,10 @@ class Appointment < ApplicationRecord
   belongs_to :pet
   belongs_to :vet
   has_many :treatments
+
+  validates :date, presence: true
+  validates :reason, presence: true
+  validates :pet, presence: true
+  validates :vet, presence: true
+  validates :status, presence: true
 end
--- vet_clinic/app/models/vet.rb
+++ vet_clinic/app/models/vet.rb
@@ -1,3 +1,8 @@
 class Vet < ApplicationRecord
     has_many :appointments
+
+validates :first_name, presence: true
+validates :last_name, presence: true
+validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+validates :specialization, presence: true
 end
